9334b3772176cc0b29d3db4f162d47aa015fc506
Jim MacArthur <jim-git@mode7.co.uk>
2013-04-27 12:40:07 +0100

    Vapour trails with rolling buffer working

--- manu.py
+++ manu.py
@@ -12,7 +12,7 @@ class Player:
         self.rot = 0
 
 def init():
-    global screen, clock, maze, shipPoly, player, speed, trailsBitmap
+    global screen, clock, maze, shipPoly, player, speed, trailsBitmap, trailsX, trailsY
     pygame.init()
     screen = pygame.display.set_mode((640,480))
     pygame.display.set_caption('Turning circle')
@@ -28,6 +28,8 @@ def init():
     trailsBitmap = pygame.Surface((640,480))
     trailsBitmap.fill((0,0,0))
     speed = 4
+    trailsX = 0
+    trailsY = 0
 
 def playerReset():
     global player
@@ -49,12 +51,24 @@ def getMaze(x,y):
     return maze[x][y]
 
 def loop():
+    global trailsX
     while 1:
         clock.tick(50)
-        screen.blit(trailsBitmap,(0,0))
+        screen.fill((127,127,127))
+        offX = int(320-player.x) % 640
+        w = 640-offX
+        offY = int(-240+player.y) % 480
+        h = 480-offY
+        screen.blit(trailsBitmap,(0,0), (w, offY, offX, h))
+        screen.blit(trailsBitmap,(offX,0),(0,offY,w,h))
+
+        screen.blit(trailsBitmap,(0,h),(w,0,640,512))
+        screen.blit(trailsBitmap,(offX, h), (0,0, 640,512))
+
+        
         dead = False
         shipRotPoly = polyRotate(shipPoly,player.rot)
-        shipTransPoly = polyTranslate(shipRotPoly,320,256)
+        shipTransPoly = polyTranslate(shipRotPoly,320,240)
         
         for x in range(int(player.x/BS)-1,int(player.x/BS)+6):
             for y in range(int(player.y/BS)-1,int(player.y/BS)+5):
@@ -63,14 +77,15 @@ def loop():
                     if polyIntersectsBlock(shipTransPoly, x*BS-player.x,y*BS-player.y):
                         dead = True
         pygame.draw.polygon(screen, (255,0,0), shipTransPoly)
-        pygame.draw.circle(trailsBitmap, (255,255,255), (320,256), 8)
+        pygame.draw.circle(trailsBitmap, (255,255,255), (320,240), 8)
         pygame.display.flip()
         if(dead): 
             playerReset()
 
         dx = speed*math.cos(player.rot)
         dy = speed*math.sin(player.rot)
-        trailsBitmap.blit(trailsBitmap, (-dx,-dy))
+        pygame.draw.circle(trailsBitmap, (255,255,255), (int(player.x % 640), int(player.y%480)), 4)
+        if(player.x - (trailsX*256) > 256): trailsX+=1
         player.x += dx
         player.y += dy
         processKeys()
