573941cfcb83fd12a869de4f0b56ba4cc21651d7
Jim MacArthur <jim-git@mode7.co.uk>
2013-04-27 11:15:39 +0100

    Faster, scrolling screen

--- manu.py
+++ manu.py
@@ -9,7 +9,7 @@ class Player:
         self.y = 32*4
         self.rot = 0
 def init():
-    global screen, clock, maze, shipPoly, player
+    global screen, clock, maze, shipPoly, player, speed
     pygame.init()
     screen = pygame.display.set_mode((640,480))
     pygame.display.set_caption('Turning circle')
@@ -19,14 +19,15 @@ def init():
     random.seed(2013)
     createMaze(maze)
     shipPoly = [ (0,2), (2,-2), (0,-1), (-2,-2), (0,2) ]
-    shipPoly = polyScale(shipPoly, 32)
+    shipPoly = polyScale(shipPoly, 8)
     shipPoly = polyRotate(shipPoly, -math.pi/2 ) # Adjust so it faces to 0 radians
     player = Player()
-
+    speed = 4
 def createMaze(maze):
     for x in range(0,64):
         for y in range(0,64):
-            maze[x][y] = random.choice([0,1])
+            if(x>2 and y>2):
+                maze[x][y] = random.choice([0,1])
 
 def polyScale(poly,scale):
     newPoly = []
@@ -55,19 +56,19 @@ def processKeys():
 
 def loop():
     while 1:
-        clock.tick(25)
+        clock.tick(50)
         screen.fill((0,0,0))
         for x in range(0,64):
             for y in range(0,64):
                 if(maze[x][y] == 1):
-                    pygame.draw.rect(screen, (255,255,255), (x*64,y*64,64,64))
+                    pygame.draw.rect(screen, (255,255,255), (x*64-player.x,y*64-player.y,64,64))
         shipRotPoly = polyRotate(shipPoly,player.rot)
-        shipTransPoly = polyTranslate(shipRotPoly,player.x,player.y)
+        shipTransPoly = polyTranslate(shipRotPoly,320,256)
         
         pygame.draw.polygon(screen, (255,0,0), shipTransPoly)
         pygame.display.flip()
-        player.x += math.cos(player.rot)
-        player.y += math.sin(player.rot)
+        player.x += speed*math.cos(player.rot)
+        player.y += speed*math.sin(player.rot)
         processKeys()
 
         for event in pygame.event.get():
