50cc522f459403e368756b24e4f7a600896176a7
Jim MacArthur <jim-git@mode7.co.uk>
2013-04-27 11:00:07 +0100

    Ship translated to player position

--- manu.py
+++ manu.py
@@ -2,8 +2,13 @@ import pygame
 from pygame.locals import *
 import random
 
+class Player:
+    def __init__(self):
+        self.x = 32*4
+        self.y = 32*4
+
 def init():
-    global screen, clock, maze, shipPoly
+    global screen, clock, maze, shipPoly, player
     pygame.init()
     screen = pygame.display.set_mode((640,480))
     pygame.display.set_caption('Turning circle')
@@ -14,6 +19,8 @@ def init():
     createMaze(maze)
     shipPoly = [ (0,2), (2,-2), (0,-1), (-2,-2), (0,2) ]
     shipPoly = polyScale(shipPoly, 32)
+    player = Player()
+
 def createMaze(maze):
     for x in range(0,64):
         for y in range(0,64):
@@ -25,6 +32,12 @@ def polyScale(poly,scale):
         newPoly.append((p[0]*scale, p[1]*scale))
     return newPoly
 
+def polyTranslate(poly,x,y):
+    newPoly = []
+    for p in poly:
+        newPoly.append((p[0]+x, p[1]+y))
+    return newPoly
+
 def loop():
     while 1:
         clock.tick(25)
@@ -34,7 +47,8 @@ def loop():
                 for y in range(0,64):
                     if(maze[x][y] == 1):
                         pygame.draw.rect(screen, (255,255,255), (x*64,y*64,64,64))
-            pygame.draw.polygon(screen, (255,0,0), shipPoly)
+            shipTransPoly = polyTranslate(shipPoly,player.x,player.y)
+            pygame.draw.polygon(screen, (255,0,0), shipTransPoly)
             if event.type == QUIT:
                 exit(0)
             elif event.type == KEYDOWN:
